d6d879748ad960f642f012231a9fbd3d8ea43534
Justino Boggio <justinoboggio@gmail.com>
2025-12-10 18:31:37 -0300

    change vm sku

--- infra/main.tf
+++ infra/main.tf
@@ -186,7 +186,7 @@ resource "azurerm_linux_virtual_machine" "agent_vm" {
   name                  = "vm-jenkins-agent"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B1s" # 1 vCPU, 1GB RAM
+  size                  = "Standard_B2ms" # 2 vCPU, 8GB RAM
   admin_username        = "ubuntu"
   network_interface_ids = [azurerm_network_interface.agent_nic.id]
 
