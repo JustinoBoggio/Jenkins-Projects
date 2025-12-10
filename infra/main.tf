0512609cd7d24459041eaa01385ee411b2c9df5a
Justino Boggio <justinoboggio@gmail.com>
2025-12-10 13:24:19 -0300

    Change label for agent and added rule in nsg

--- infra/main.tf
+++ infra/main.tf
@@ -82,6 +82,17 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+      name                       = "App-Python-Port"
+      priority                   = 1010
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "5000"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
 }
 
 # Public IP of Master
