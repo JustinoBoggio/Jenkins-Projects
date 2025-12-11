6312db903e8ad7f0d9b9a10df67928f9dcea1a4c
Justino Boggio <justinoboggio@gmail.com>
2025-12-11 12:00:40 -0300

    Added grafana and prometheus port exposure

--- infra/main.tf
+++ infra/main.tf
@@ -115,6 +115,28 @@ resource "azurerm_network_security_group" "nsg" {
       source_address_prefix      = "*"
       destination_address_prefix = "*"
     }
+  security_rule {
+      name                       = "Prometheus-Port"
+      priority                   = 1040
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "9090"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
+  security_rule {
+      name                       = "Grafana-Port"
+      priority                   = 1050
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "3200"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
 }
 
 # Public IP of Master
